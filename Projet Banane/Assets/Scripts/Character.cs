b90c78f7294f9754941101c27a990bd0df46d1be
Thibault Antunes <thibaultantunes@free.fr>
2016-04-16 19:14:12 +0200

    Gameplay mis a jour + perso part dans l'espace
    nâeinbg^ùaigbùag

--- Projet Banane/Assets/Scripts/Character.cs
+++ Projet Banane/Assets/Scripts/Character.cs
@@ -18,13 +18,32 @@ public class Character : MonoBehaviour {
     private bool superHit;
     private bool runHit;
     private bool BackHit;
+    private bool touched;
+    protected bool activeBlock;
+    protected bool dead;
+    private Vector3 moveDirection;
+
+
+    //Variables pour le squelette
+    protected Transform hips;
+    protected Transform leftUpLeg;
+    protected Transform leftLeg;
+    protected Transform rightUpLeg;
+    protected Transform rightLeg;
+    protected Transform spine;
+    protected Transform head;
+    protected Transform rightArm;
+    protected Transform rightForeArm;
+    protected Transform leftArm;
+    protected Transform leftForeArm;
+    protected Transform[] body;
 
 
     // Use this for initialization
     void Start ()
     {
         lives = 100;
-        block = 100;
+        block = 1000;
         isBlocking = false;
         combo = 0;
         stunned = false;
@@ -38,85 +57,172 @@ public class Character : MonoBehaviour {
         superHit = false;
         runHit = false;
         BackHit = false;
+        touched = false;
+        activeBlock = false;
+        dead = false;
+
+
+        //Assignation du Ragdoll
+
+       body = new Transform[11];
+        hips = transform.Find("Armature").Find("Hips").transform;
+        spine = transform.Find("Armature").Find("Hips").Find("Spine");
+        body[0] = hips;
+        leftUpLeg = hips.Find("LeftUpLeg");
+        body[1] = leftUpLeg;
+        leftLeg = leftUpLeg.Find("LeftLeg");
+        body[2] = leftLeg;
+        rightUpLeg = hips.Find("RightUpLeg");
+        body[3] = rightUpLeg;
+        rightLeg = rightUpLeg.Find("RightLeg");
+        body[4] = rightLeg;
+        body[5] = spine;
+        head = spine.Find("Spine1").Find("Spine2").Find("Neck").Find("Head");
+        body[6] = head;
+        rightArm = spine.Find("Spine1").Find("Spine2").Find("RightShoulder").Find("RightArm");
+        body[7] = rightArm;
+        rightForeArm = rightArm.Find("RightForeArm");
+        body[8] = rightForeArm;
+        leftArm = spine.Find("Spine1").Find("Spine2").Find("LeftShoulder").Find("LeftArm");
+        body[9] = leftArm;
+        leftForeArm = leftArm.Find("LeftForeArm");
+        body[10] = leftForeArm;
+
+        //Desactivation du Ragdoll au début pour que le perso ne s'ecrase pas
+
+        for (int i = 0; i < body.Length; i++)
+        {
+            body[i].GetComponent<Rigidbody>().mass = 400.0f;
+            body[i].GetComponent<Rigidbody>().isKinematic = true;
+
+        }
+
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if(this.gameObject.name == "J1")
-        {
-            J1();
-        }
-        else
-        {
-            J2();
-        }
 
-        
+        moveDirection = new Vector3(0, 0, 0);
+      
 
-	    if(!isBlocking && !stunned && !run && !back &&!hit &&!loadingSuperHit &&!superHit &&!runHit && !BackHit)
+        if(!dead)
         {
-            this.transform.GetComponent<Animation>().CrossFade("Iddle.001");
-        }
-        else
-        {
-            if (run &&!BackHit &&!runHit)
+
+            if (block < 100 && !isBlocking)
             {
-                this.transform.GetComponent<Animation>().CrossFade("Front");
+                block = block + 1;
             }
-            if(back)
+
+            if (this.gameObject.name == "J1")
             {
-                this.transform.GetComponent<Animation>().CrossFade("Back");
+                J1();
             }
-            if(isBlocking)
+            else
             {
-                this.transform.GetComponent<Animation>().CrossFade("Block");
+                J2();
             }
-            if(stunned)
+
+            if (!isBlocking && !stunned && !run && !back && !hit && !loadingSuperHit && !superHit && !runHit && !BackHit && !touched && !activeBlock)
             {
-                this.transform.GetComponent<Animation>().CrossFade("Stunned");
+                this.transform.GetComponent<Animation>().CrossFade("Iddle.001");
             }
-            if(hit)
+            else
             {
-                switch(combo)
+                if (run && !BackHit && !runHit)
                 {
-                    case 1:
-                        this.transform.GetComponent<Animation>().CrossFade("Hit1");
-                        break;
-                    case 2:
-                        this.transform.GetComponent<Animation>().CrossFade("Hit2");
-                        break;
-                    case 3:
-                        this.transform.GetComponent<Animation>().CrossFade("Hit3");
-                        break;
-                    case 4:
-                        this.transform.GetComponent<Animation>().CrossFade("Hit4");
-                        break;
-                    default:
-                        break;
+                    this.transform.GetComponent<Animation>().CrossFade("Front");
+                }
+                if (back)
+                {
+                    this.transform.GetComponent<Animation>().CrossFade("Back");
+                }
+                if (isBlocking)
+                {
+                    this.block = block - 5;
+                    this.transform.GetComponent<Animation>().CrossFade("Block");
+                }
+                if (stunned)
+                {
+                    goBack();
+                    this.transform.GetComponent<Animation>().CrossFade("Stunned");
+                }
+                if (hit)
+                {
+                    switch (combo)
+                    {
+                        case 1:
+                            this.transform.GetComponent<Animation>().CrossFade("Hit1");
+                            break;
+                        case 2:
+                            this.transform.GetComponent<Animation>().CrossFade("Hit2");
+                            break;
+                        case 3:
+                            this.transform.GetComponent<Animation>().CrossFade("Hit3");
+                            break;
+                        case 4:
+                            this.transform.GetComponent<Animation>().CrossFade("Hit4");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                if (loadingSuperHit)
+                {
+                    this.transform.GetComponent<Animation>().CrossFade("LoadSuperHit");
+                }
+                if (superHit)
+                {
+                    this.transform.GetComponent<Animation>().CrossFade("SuperHit");
+                }
+                if (touched)
+                {
+                    this.transform.GetComponent<Animation>().CrossFade("Touched");
+                }
+                if (stunned)
+                {
+                    this.transform.GetComponent<Animation>().CrossFade("Stunned");
+                }
+                if (activeBlock)
+                {
+                    isBlocking = false;
+                    this.transform.GetComponent<Animation>().CrossFade("ActiveBlock");
+                }
+
+                if (runHit)
+                {
+                    this.transform.GetComponent<Animation>().CrossFade("RunHit");
+                }
+
+                if (BackHit)
+                {
+                    this.transform.GetComponent<Animation>().CrossFade("BackHit");
                 }
             }
-            if(loadingSuperHit)
-            {
-                this.transform.GetComponent<Animation>().CrossFade("LoadSuperHit");
-            }
-            if (superHit)
-            {
-                this.transform.GetComponent<Animation>().CrossFade("SuperHit");
-            }
 
-
-            if(runHit)
-            {
-                this.transform.GetComponent<Animation>().CrossFade("RunHit");
-            }
-
-            if(BackHit)
-            {
-                this.transform.GetComponent<Animation>().CrossFade("BackHit");
-            }
+            moveDirection.y = -1;
+            controller.Move(moveDirection);
         }
+        else
+        {
+           // this.transform.GetComponent<Character>().enabled = false;
+            this.transform.GetComponent<CharacterController>().enabled = false;
+        }
+
+
+	   
 	}
+
+    public void impact(int damages)
+    {
+
+        Transform spawnImpact;
+        spawnImpact = transform.Find("ImpactZone");
+
+                GameObject clone = Instantiate(Resources.Load("Prefabs/Impact"), spawnImpact.transform.position, spawnImpact.transform.rotation) as GameObject;
+                clone.GetComponent<Damages>().setDamages(damages);
+                clone.gameObject.GetComponent<Damages>().setOwner(this.gameObject.name);
+    }
 
     public void nextCombo()
     {
@@ -147,11 +253,13 @@ public class Character : MonoBehaviour {
         back = true;
         if (this.gameObject.name == "J1")
         {
-            this.GetComponent<CharacterController>().Move(new Vector3(0, 0, 0.1f));
+            moveDirection.z = 0.1f;
+            //this.GetComponent<CharacterController>().Move(new Vector3(0, 0, 0.1f));
         }
         else
         {
-            this.GetComponent<CharacterController>().Move(new Vector3(0, 0, -0.1f));
+            moveDirection.z = -0.1f;
+            //this.GetComponent<CharacterController>().Move(new Vector3(0, 0, -0.1f));
         }
             
     }
@@ -161,19 +269,19 @@ public class Character : MonoBehaviour {
         run = true;
         if(this.gameObject.name == "J1")
         {
-            this.GetComponent<CharacterController>().Move(new Vector3(0, 0, -0.1f));
+            moveDirection.z = -0.1f;
+          // this.GetComponent<CharacterController>().Move(new Vector3(0, 0, -0.1f));
         }
         else
         {
-            this.GetComponent<CharacterController>().Move(new Vector3(0, 0, 0.1f));
+            moveDirection.z = 0.1f;
+           // this.GetComponent<CharacterController>().Move(new Vector3(0, 0, 0.1f));
         }
         
     }
 
     public void strike()
     {
-
-        Debug.Log("Attack" + this.gameObject.name);
 
         if (run)
         {
@@ -203,6 +311,23 @@ public class Character : MonoBehaviour {
         }
     }
 
+    public void allFalse()
+    {
+        
+        run = false;
+        back = false;
+        hit = false;
+        loadingSuperHit = false;
+        superHit = false;
+        runHit = false;
+        BackHit = false;
+        isBlocking = false;
+        stunned = false;
+        touched = false;
+        activeBlock = false;
+
+    }
+
     public void J1()
     {
 
@@ -227,7 +352,7 @@ public class Character : MonoBehaviour {
             run = false;
         }
 
-        if (Input.GetKey(KeyCode.Z))
+        if (Input.GetKey(KeyCode.Z) && block > 0)
         {
             isBlocking = true;
         }
@@ -280,7 +405,7 @@ public class Character : MonoBehaviour {
             run = false;
         }
 
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) && block > 10)
         {
             isBlocking = true;
         }
@@ -310,23 +435,67 @@ public class Character : MonoBehaviour {
         
     }
 
-    public void attack()
+    public void hardStun()
     {
-
+        allFalse();
+        stunned = true;
     }
 
-    public void loadSuperAttack()
+    public void takeDamages(int damages)
     {
+        allFalse();
+        if(!isBlocking)
+        {
+            if (damages >= 15)
+            {
+                hardStun();
+            }
+            else
+            {
+                touched = true;
+            }
+            
+            this.lives = lives - damages;
 
-    }
-
-    public void move()
-    {
-
+            if(this.lives <= 0)
+            {
+                this.lives = 0;
+                die();
+            }
+        }
+        else
+        {
+            this.block = block - damages;
+            if (damages >= 15)
+            {
+                hardStun();
+            }
+            else
+            {
+                isBlocking = false;
+                activeBlock = true;
+            }
+            
+        }
     }
 
     public void die()
     {
+        
+        this.transform.GetComponent<Animation>().enabled = false;
+        //   this.GetComponent<CharacterController>().enabled = false;
+        dead = true;
 
+        for (int i = 0; i < body.Length - 1; i++)
+        {
+
+            body[i].GetComponent<Rigidbody>().isKinematic = false;
+            body[i].GetComponent<Rigidbody>().useGravity = true;
+          /*  if (body[i].GetComponent<CharacterJoint>() != null)
+            {
+
+                body[i].GetComponent<CharacterJoint>().gameObject.SetActive(true);
+            }*/
+        }
     }
 }
---
+++ Projet Banane/Assets/Scripts/Damages.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damages : MonoBehaviour {
+
+    string owner;
+    int damages;
+    int timer;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+
+    public void setDamages(int _damages)
+    {
+        this.damages = _damages;
+    }
+
+    public void setOwner(string _owner)
+    {
+        this.owner = _owner;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        timer++;
+        if(timer == 10)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.tag == "Fighter" && collision.gameObject.name != owner)
+        {
+            collision.gameObject.GetComponent<Character>().takeDamages(this.damages);
+        }
+    }
+}
