c192c8c9da75f07ae0fdb356ea843b21fee5029d
Thibault Antunes <thibaultantunes@free.fr>
2016-04-18 10:44:42 +0200

    Menu Principal

---
+++ Projet Banane/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	
+
+    void Start()
+    {
+        hideInstructions();
+    }
+
+    public void hideInstructions()
+    {
+        transform.Find("infos").gameObject.SetActive(false);
+    }
+
+    public void showInstructions()
+    {
+        transform.Find("infos").gameObject.SetActive(true);
+    }
+
+    public void launchGame()
+    {
+        Application.LoadLevel("tests Titix");
+    }
+
+
+}
